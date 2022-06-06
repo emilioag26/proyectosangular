96ccb42a7012c745ae80838ccf0c95e3125d4b7c
EmilioAG99 <luis.emilio99@hotmail.com>
2022-06-06 11:35:54 -0500

    courses

---
+++ html-css/runJs.js
@@ -0,0 +1,13 @@
+const cat = {
+    name: "miso",
+    meow()
+    {
+        console.log(this.name)
+    }
+}
+cat.meow()
+let array = [1,2,3,4,5,6,7,8,9,10]
+
+array.forEach((number)=>{
+    console.log(number)
+})
